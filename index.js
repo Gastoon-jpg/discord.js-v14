0d4b5b3b7fe0dfbf0a231ddf1cd964710e9f4810
Gastoon <74118841+Gastoon-jpg@users.noreply.github.com>
2022-09-07 20:18:16 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const { GatewayIntentBits, EnumResolvers } = require("discord.js");
+const { readdirSync } = require("fs");
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+
+const botToken = "token";
+
+client.on('ready', () => {
+   console.log(`¡Estoy listo!`);
+});
+
+client.slashCommands = new Discord.Collection();
+
+// Slash commands
+for (const subFolder of readdirSync(`${__dirname}/slashCommands/`)) {
+    for (const fileName of readdirSync(`${__dirname}/slashCommands/${subFolder}/`)) {
+        let file = require(`${__dirname}/slashCommands/${subFolder}/${fileName}`);
+       client.slashCommands.set(file.name, file);
+    }
+}
+
+// Eventos
+for (const fileName of readdirSync(`${__dirname}/eventos/`)) {
+    let file = require(`${__dirname}/eventos/${fileName}`);
+    let eventEmiter = file.emiter;
+
+    client[eventEmiter](file.name, file.run.bind(null, client));
+}
+
+client.login(botToken);
